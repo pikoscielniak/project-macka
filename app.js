6fc91f35e79af5d685f585c0e9de6545daaaae3b
piotrekbass <piotrek.koscielniak@gmail.com>
2014-02-27 23:25:09 +0100

    added bower to modules to enable heroku loading bower dependencies

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 var koa = require('koa');
 var serve = require('koa-static');
 var app = module.exports = koa();
-var path = require('path');
-var extname = path.extname;
-var fs = require('fs');
 var favicon = require('koa-favicon');
 
 app.use(favicon());
@@ -11,9 +8,7 @@ app.use(serve(__dirname + "/public"));
 
 
 app.use(function *(){
-    var path = __dirname + this.path;
-    this.type = extname(path);
-    this.body = fs.createReadStream(path);
+    this.body = 'Choose app';
 });
 
 var port = process.env.PORT || 8000;
